a8e87a201a37eb55e57635f0b88f5c0f9e4e9182
Edward He <edward.he@macaulay.cuny.edu>
2018-04-22 08:59:51 -0400

    Refactor List.

--- src/components/List.js
+++ src/components/List.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class List extends Component {
-  constructor(props) {
-    super(props);
-  }
+const List = ({ words, setWord }) => (
+  <ul>
+    {
+      words.map(word => (
+        <li key={word} >
+          <span
+            onClick={() => {setWord(word)}}
+          >
+            {word}
+          </span>
+        </li>
+      ))
+    }
+  </ul>
+);
 
-  render() {
-    const { words } = this.props;
-    return (
-      <ul>
-        {
-          words.map(word => (
-            <li key={word} >
-              <span
-                onClick={() => {this.props.setWord(word)}}
-              >
-                {word}
-              </span>
-            </li>
-          ))
-        }
-      </ul>
-    );
-  }
-}
+List.propTypes = {
+  words: PropTypes.array.isRequired,
+  setWord: PropTypes.func.isRequired,
+};
+
+export default List;
